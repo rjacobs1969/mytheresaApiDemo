2feaa907393462620cf32b22665ec7f6662d5ebd
rjacobs1969 <rjacobs@atrapalo.com>
2024-12-05 11:18:10 +0100

    fix: update comment in DiscountRepositoryInterface for clarity on future implementation

--- app/src/Domain/Discount/DiscountRepositoryInterface.php
+++ app/src/Domain/Discount/DiscountRepositoryInterface.php
@@ -9,7 +9,7 @@ use App\Domain\Product\ProductFilterCollection;
 interface DiscountRepositoryInterface
 {
     public function find(): DiscountCollection;
-    //Not in current assignment scope, but would be implemented in a real-world scenario:
+    //Not in current assignment scope, but would (probably) be implemented in a real-world scenario:
     //public function save(Discount $discount): void;
     //public function update(Discount $discount): void;
     //public function delete(Discount $discount): void;
